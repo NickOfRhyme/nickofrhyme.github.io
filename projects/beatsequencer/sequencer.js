c43f36d472119b91216f4c1fb62eccdfe7d6715b
NickOfRhyme <nhwoolrich@gmail.com>
2020-03-26 12:03:08 +0000

    uploading beatsequencer

---
+++ projects/beatsequencer/sequencer.js
@@ -0,0 +1,87 @@
+let beatInterval;
+let currBeatNum = 0;
+let beatsPM = 120;
+const totalBeats = 8;
+const playPauseButton = document.querySelector("button.play");
+const stopButton = document.querySelector("button.stop");
+const beatsPMslider = document.querySelector("#BPM-slider");
+const beatsPMdisplay = document.querySelector("#BPM-display > #value");
+const drums = document.querySelectorAll("#container > *");
+
+playPauseButton.addEventListener("click", playPauseToggle);
+stopButton.addEventListener("click", stopBeat);
+beatsPMslider.addEventListener("input", changeBPM);
+drums.forEach(drum => {
+  drum.addEventListener("click", drumChoiceToggle);
+});
+
+function startBeat(beatsPM) {
+  beatInterval = setInterval(advanceBeat, (60 / beatsPM) * 1000);
+  beatsPMslider.disabled = !beatsPMslider.disabled;
+}
+
+function advanceBeat() {
+  if (currBeatNum >= 1) {
+    const lastBeatColumn = document.querySelectorAll(`.beat${currBeatNum}`);
+    lastBeatColumn.forEach(beat => {
+      beat.classList.remove("playing");
+    });
+  }
+
+  currBeatNum === totalBeats ? (currBeatNum = 1) : currBeatNum++;
+
+  const currBeatColumn = document.querySelectorAll(`.beat${currBeatNum}`);
+
+  currBeatColumn.forEach(beat => {
+    beat.classList.add("playing");
+    if (beat.classList.contains("cued")) playSound(beat.dataset.drum);
+  });
+}
+
+function pauseBeat() {
+  clearInterval(beatInterval);
+  beatsPMslider.disabled = !beatsPMslider.disabled;
+}
+
+function stopBeat() {
+  pauseBeat();
+  const currBeatColumn = document.querySelectorAll(`.beat${currBeatNum}`);
+  currBeatColumn.forEach(beat => {
+    beat.classList.remove("playing");
+  });
+
+  if (playPauseButton.classList.contains("pause")) {
+    playPauseToggle({ target: playPauseButton });
+  }
+
+  currBeatNum = 0;
+}
+
+function playSound(drumType) {
+  console.log(drumType);
+  const sound = document.querySelector(`#${drumType}`);
+  sound.pause();
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function drumChoiceToggle({ target: { classList } }) {
+  classList.toggle("cued");
+}
+
+function playPauseToggle({ target: { classList } }) {
+  if (classList.contains("play")) {
+    classList.remove("play");
+    classList.add("pause");
+    startBeat(beatsPM);
+  } else {
+    classList.remove("pause");
+    classList.add("play");
+    pauseBeat();
+  }
+}
+
+function changeBPM({ target: { value } }) {
+  beatsPMdisplay.textContent = value;
+  beatsPM = +value;
+}
